0f080b9bfbb3cfc0743a6e063ba01168d53edc11
Neeraj <eng20cs0301.neeraj@gmail.com>
2024-11-21 17:37:40 +0530

    forth commit

--- vite.config.js
+++ vite.config.js
@@ -5,17 +5,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build',
-    rollupOptions: {
-      output: {
-        // Manual chunking to reduce chunk sizes
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return id.split('node_modules/')[1].split('/')[0]; // Split by package
-          }
-        },
-      },
-    },
-    chunkSizeWarningLimit: 600, // Increase the warning limit if necessary
+    outDir: 'dist', // Ensure this matches what you want Vercel to look for
   },
 });
